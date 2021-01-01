586a68879bc36330ed3f892d76df328713a8e5a3
iseeyoucopy <iseeyoucopy@users.noreply.github.com>
2021-01-01 22:16:55 +0000

    update

--- include/cleanup/freeuser_update.php
+++ include/cleanup/freeuser_update.php
@@ -17,7 +17,8 @@ function docleanup($data)
     ignore_user_abort(1);
     //=== Free user removal by Bigjoos/pdq:)
     $res = sql_query("SELECT id, modcomment FROM users WHERE free_switch > 1 AND free_switch < " . TIME_NOW) or sqlerr(__FILE__, __LINE__);
-    $msgs_buffer = $users_buffer = array();
+    $msgs_buffer = array();
+	$users_buffer = array();
     if (mysqli_num_rows($res) > 0) {
         $subject = "Freeleech expired.";
         $msg = "Your freeleech has expired and has been auto-removed by the system.\n";
@@ -25,7 +26,7 @@ function docleanup($data)
             $modcomment = $arr['modcomment'];
             $modcomment = get_date(TIME_NOW, 'DATE', 1) . " - Freeleech Removed By System.\n" . $modcomment;
             $modcom = sqlesc($modcomment);
-			$msgs_buffer[] = "(0, " . $arr['id'] . ", " . TIME_NOW . ", " . sqlesc($msg) . ", " . sqlesc($subject) . ")";
+			$msgs_buffer[] = "(\"0\", " . $arr['id'] . ", " . TIME_NOW . ", " . sqlesc($msg) . ", " . sqlesc($subject) . ")";
 			
             $users_buffer[] = "(" . $arr['id'] . ", 0, " . $modcom . ")";
             $cache->update_row('user' . $arr['id'], [
@@ -42,7 +43,7 @@ function docleanup($data)
         }
         $count = count($users_buffer);
         if ($count > 0) {
-            sql_query("INSERT INTO messages (sender, receiver, added, msg, subject) VALUES (" . implode(', ', $msgs_buffer) . ")") or sqlerr(__FILE__, __LINE__);
+			sql_query("INSERT INTO messages (sender, receiver, added, msg, subject) VALUES " . implode(', ', $msgs_buffer)) or sqlerr(__FILE__, __LINE__);
             sql_query("INSERT INTO users (id, free_switch, modcomment) VALUES " . implode(', ', $users_buffer) . " ON DUPLICATE key UPDATE free_switch=values(free_switch), modcomment=values(modcomment)") or sqlerr(__FILE__, __LINE__);
             write_log("Cleanup - Removed Freeleech from " . $count . " members");
         }
