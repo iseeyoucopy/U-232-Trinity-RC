81d794ffc2c7eb9f12708f247bd9083920ab6e2c
iseeyoucopy <iseeyoucopy@users.noreply.github.com>
2021-01-09 09:32:01 +0000

    updated missing code

--- install/functions/database.php
+++ install/functions/database.php
@@ -12,13 +12,13 @@
  */
 function db_test()
 {
-    global $root, $TRINITY;
+    global $root, $TRINITY20;
     $out = '<fieldset><legend>Database</legend>';
     require_once ($root.'include/config.php');
-    $mysqli_test = new mysqli($TRINITY['mysql_host'], $TRINITY['mysql_user'], $TRINITY['mysql_pass'], $TRINITY['mysql_db']);
+    $mysqli_test = new mysqli($TRINITY20['mysql_host'], $TRINITY20['mysql_user'], $TRINITY20['mysql_pass'], $TRINITY20['mysql_db']);
     if (!$mysqli_test->connect_error) {
         $out.= '<div class="readable">Connection to database was made</div>';
-        if ($mysqli_test->select_db($TRINITY['mysql_db'])) {
+        if ($mysqli_test->select_db($TRINITY20['mysql_db'])) {
             $out.= '<div class="readable">Data base exists, data can be imported</div>';
             $out.= '<form action="index.php" method="post"><div class="info" style="text-align:center;"><input type="hidden" name="do" value="db_insert" /><input type="hidden" name="xbt" value="'.$_GET['xbt'].'" /><input type="submit" value="Import database" /></div></form>';
         } else $out.= '<div class="notreadable">There was an error while selecting the database<br/>'.$mysqli_test->error.'</div><div class="info" style="text-align:center"><input type="button" value="Reload" onclick="window.location.reload()"/></div>';
@@ -29,13 +29,13 @@ function db_test()
 //== Win - remember to set your path up correctly in the query- atm its set for appserv c:\AppServ\MySQL\bin\mysql
 function db_insert()
 {
-    global $root, $TRINITY;
+    global $root, $TRINITY20;
     $out = '<fieldset><legend>Database</legend>';
     require_once ($root.'include/config.php');
     if($_POST['xbt'] == 1) $file = "install.xbt.sql";
     elseif($_POST['xbt'] == 0) $file = "install.php.sql";
-    $q = sprintf('/usr/bin/mysql -h %s -u %s -p%s %s < %sinstall/extra/'.$file, $TRINITY['mysql_host'], $TRINITY['mysql_user'], $TRINITY['mysql_pass'], $TRINITY['mysql_db'], $root); //== Linux
-    //$q = sprintf('c:\AppServ\MySQL\bin\mysql -h %s -u %s -p%s %s < %sinstall/extra/'.$file,$TRINITY['mysql_host'],$TRINITY['mysql_user'],$TRINITY['mysql_pass'],$TRINITY['mysql_db'],$root);  //== Win - remember to set your path up correctly - atm its set for appserv
+    $q = sprintf('/usr/bin/mysql -h %s -u %s -p%s %s < %sinstall/extra/'.$file, $TRINITY20['mysql_host'], $TRINITY20['mysql_user'], $TRINITY20['mysql_pass'], $TRINITY20['mysql_db'], $root); //== Linux
+    //$q = sprintf('c:\AppServ\MySQL\bin\mysql -h %s -u %s -p%s %s < %sinstall/extra/'.$file,$TRINITY20['mysql_host'],$TRINITY20['mysql_user'],$TRINITY20['mysql_pass'],$TRINITY20['mysql_db'],$root);  //== Win - remember to set your path up correctly - atm its set for appserv
     exec($q, $o);
     if (!count($o)) {
         $out.= '<div class="readable">Database was imported</div><div class="info" style="text-align:center"><input type="button" value="Finish" onclick="window.location.href=\'?step=3\'"/></div>';
