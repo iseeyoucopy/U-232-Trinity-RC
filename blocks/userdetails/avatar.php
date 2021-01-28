79b236e187b0a50507affd38c91338df87bf1c1c
iseeyoucopy <iseeyoucopy@users.noreply.github.com>
2021-01-28 21:29:16 +0000

    default avatar

--- blocks/userdetails/avatar.php
+++ blocks/userdetails/avatar.php
@@ -14,7 +14,7 @@ if ($user["avatar"])
 	$HTMLOUT.= "<tr><td class='rowhead'>{$lang['userdetails_avatar']}</td><td align='left'><img class='img-polaroid' src='" . htmlsafechars($user["avatar"]) . "'></td></tr>\n";
 else
 	$HTMLOUT.= "
-	<tr><td class='rowhead'>{$lang['userdetails_avatar']}</td><td align='left'><img class='img-polaroid' src='{$TRINITY20['pic_base_url']}forumicons/default_avatar.gif'></td></tr>\n";
+	<tr><td class='rowhead'>{$lang['userdetails_avatar']}</td><td align='left'><img class='img-polaroid' src='{$TRINITY20['pic_base_url']}default_avatar.gif'></td></tr>\n";
 //==end
 // End Class
 // End File
