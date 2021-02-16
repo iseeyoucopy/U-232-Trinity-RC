5876da7d30cd00f326816c83e46d555f6773073c
HDVinnie <hdinnovations@protonmail.com>
2021-02-16 11:37:57 -0500

    refactor: ajax.php

--- ajax.php
+++ ajax.php
@@ -5,12 +5,12 @@ require_once INCL_DIR.'user_functions.php';
 dbconn(true);
 loggedinorreturn();
     if (($unread_m = $cache->get('inbox_new::' . $CURUSER['id'])) === false) {
-        $res = sql_query('SELECT count(id) FROM messages WHERE receiver=' . sqlesc($CURUSER['id']) . ' && unread="yes" AND location = "1"') or sqlerr(__FILE__, __LINE__);
+        ($res = sql_query('SELECT count(id) FROM messages WHERE receiver=' . sqlesc($CURUSER['id']) . ' && unread="yes" AND location = "1"')) || sqlerr(__FILE__, __LINE__);
         $arr = $res->fetch_row();
         $unread_m = (int)$arr[0];
         $cache->set('inbox_new::' . $CURUSER['id'], $unread_m, $TRINITY20['expires']['unread']);
 }
-$result = sql_query("SELECT COUNT(id) FROM messages WHERE receiver = " . sqlesc($CURUSER['id']) . "  AND unread = 'yes' AND location = '1'") or sqlerr(__FILE__, __LINE__);
+($result = sql_query("SELECT COUNT(id) FROM messages WHERE receiver = " . sqlesc($CURUSER['id']) . "  AND unread = 'yes' AND location = '1'")) || sqlerr(__FILE__, __LINE__);
 $htmlout = '';
 if (isset($unread_m) && !empty($unread_m)) {
     $htmlout .= "<span class='badge warning'>" . $unread_m . "</span>";
