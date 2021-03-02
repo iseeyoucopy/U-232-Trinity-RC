58bc87e58ed9b650eb5382a5e9ceb98c1ed7b120
iseeyoucopy <iseeyoucopy@users.noreply.github.com>
2021-03-02 20:45:21 +0000

    SaSS and JS changes using GULP,webpack,BrowserSync

---
+++ gulpfile.babel.js
@@ -0,0 +1,141 @@
+'use strict';
+
+const plugins = require ('gulp-load-plugins');
+const yargs = require('yargs');
+const browser = require('browser-sync');
+const gulp = require('gulp');
+const rimraf = require('rimraf');
+const yaml = require('js-yaml');
+const fs = require('fs');
+const webpackStream = require('webpack-stream');
+const webpack2 = require ('webpack');
+const named = require('vinyl-named');
+const autoprefixer = require('autoprefixer');
+
+// Load all Gulp plugins into one variable
+const $ = plugins();
+
+// Check for --production flag
+const PRODUCTION = !!(yargs.argv.production);
+
+// Load settings from settings.yml
+const { PORT, PATHS } = loadConfig();
+
+function loadConfig() {
+  let ymlFile = fs.readFileSync('config.yml', 'utf8');
+  return yaml.load(ymlFile);
+}
+
+// Build the "dist" folder by running all of the below tasks
+// Sass must be run later so UnCSS can search for used classes in the others assets.
+gulp.task('build',
+ gulp.series(clean, gulp.parallel(javascript, images, webfonts, copy), sass));
+
+// Build the site, run the server, and watch for file changes
+gulp.task('default',
+  gulp.series('build', server, watch));
+
+// Delete the "dist" folder
+// This happens every time a build starts
+function clean(done) {
+  rimraf(PATHS.dist, done);
+}
+
+// Copy files out of the assets folder
+// This task skips over the "img", "js", and "scss" folders, which are parsed separately
+function copy() {
+  return gulp.src(PATHS.assets)
+    .pipe(gulp.dest(PATHS.dist + ''));
+}
+
+// Compile Sass into CSS
+// In production, the CSS is compressed
+function sass() {
+  const postCssPlugins = [
+    // Autoprefixer
+    autoprefixer(),
+
+    // UnCSS - Uncomment to remove unused styles in production
+    // PRODUCTION && uncss.postcssPlugin(UNCSS_OPTIONS),
+  ].filter(Boolean);
+  return gulp.src('scss/app.scss')
+    .pipe($.sourcemaps.init())
+    .pipe($.sass({
+      includePaths: PATHS.sass
+    })
+      .on('error', $.sass.logError))
+    .pipe($.postcss(postCssPlugins))
+    .pipe($.if(PRODUCTION, $.cleanCss({ compatibility: 'ie9' })))
+    .pipe($.if(!PRODUCTION, $.sourcemaps.write()))
+    .pipe(gulp.dest(PATHS.dist + '/css'))
+    .pipe(browser.reload({ stream: true }));
+}
+
+let webpackConfig = {
+  mode: (PRODUCTION ? 'production' : 'development'),
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [ "@babel/preset-env" ],
+            compact: false
+          }
+        }
+      }
+    ]
+  },
+  devtool: !PRODUCTION && 'source-map'
+}
+
+// Combine JavaScript into one file
+// In production, the file is minified
+function javascript() {
+  return gulp.src('scripts/app.js')
+    .pipe(named())
+    .pipe($.sourcemaps.init())
+    .pipe(webpackStream(webpackConfig, webpack2))
+    .pipe($.if(PRODUCTION, $.terser()
+      .on('error', e => { console.log(e); })
+    ))
+    .pipe($.if(!PRODUCTION, $.sourcemaps.write()))
+    .pipe(gulp.dest(PATHS.dist + '/js'));
+}
+
+// Copy images to the "dist" folder
+// In production, the images are compressed
+function images() {
+  return gulp.src('images/')
+    .pipe($.if(PRODUCTION, $.imagemin([
+      $.imagemin.jpegtran({ progressive: true }),
+    ])))
+    .pipe(gulp.dest(PATHS.dist + '/img'));
+}
+function webfonts() {
+  return gulp.src('scss/fontawesome/webfonts/*')
+    .pipe($.if(PRODUCTION, $.sourcemaps.write()))
+    .pipe(gulp.dest(PATHS.dist + '/webfonts'));
+};
+// Start a server with BrowserSync to preview the site in
+function server(done) {
+  browser.init({
+    server: PATHS.dist, port: PORT
+  }, done);
+}
+
+// Reload the browser with BrowserSync
+function reload(done) {
+  browser.reload();
+  done();
+}
+
+// Watch for changes to static assets, pages, Sass, and JavaScript
+function watch() {
+  gulp.watch(PATHS.assets, copy);
+  gulp.watch('scss/**/*.scss').on('all', sass);
+  gulp.watch('scripts/**/*.js').on('all', gulp.series(javascript, browser.reload));
+  gulp.watch('images/**/*').on('all', gulp.series(images, browser.reload));
+  gulp.watch('webfonts/**/*').on('all', gulp.series(webfonts, browser.reload));
+}
--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,32 @@
-$(document).foundation()
+import $ from 'jquery';
+import 'what-input';
+// Foundation JS relies on a global variable. In ES6, all imports are hoisted
+// to the top of the file so if we used `import` to import Foundation,
+// it would execute earlier than we have assigned the global variable.
+// This is why we have to use CommonJS require() here since it doesn't
+// have the hoisting behavior.
+window.jQuery = $;
+require('foundation-sites');
+// If you want to pick and choose which modules to include, comment out the above and uncomment
+// the line below
+//import './lib/foundation-explicit-pieces';
+$(document).foundation();
+
+$('[data-open-details]').click(function (e) {
+  e.preventDefault();
+  $(this).next().toggleClass('is-active');
+  $(this).toggleClass('is-active');
+});
+$(function(){  
+  $.ajax({
+      type: 'GET',
+      url: 'ajax.php',
+      success: function(data){
+          $('#unread_m').append(data);
+      }
+  });
+  setInterval(function(){//setInterval() method execute on every interval until called clearInterval()
+          $('#unread_m').load('ajax.php').fadeIn('slow');
+          //load() method fetch data from fetch.php page
+          }, 1000);    
+  });
