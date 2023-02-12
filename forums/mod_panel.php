d9e76e8d19b5599ec2ab5729496f910a25b8f43f
iseeyoucopy <iseeyoucopy@yahoo.com>
2023-02-12 19:22:48 +0000

    update : added inline labels instead of table row

--- forums/mod_panel.php
+++ forums/mod_panel.php
@@ -27,67 +27,54 @@ if (!defined('IN_TRINITY20_FORUM')) {
 $lang = array_merge($lang, load_language('ad_modpanel'));
 
 if ($CURUSER['class'] >= UC_STAFF || isMod($forumid, "forum")) {
-    $HTMLOUT .= "<form method='post' action='forums.php'>
-	 <input type='hidden' name='action' value='updatetopic'>
-	 <input type='hidden' name='topicid' value='{$topicid}'>";
-    /*$HTMLOUT .= begin_table();*/
-    $HTMLOUT .= "<table class='table table-hover table-bordered'>
-	 <tr>
-	 <td colspan='2' class='colhead'>{$lang['mod_mp_opt']}</td>
-	 </tr>
-	 <tr>
-	 <td class='rowhead' width='1%'>{$lang['mod_mp_stk']}</td>
-	 <td>
-	 <select name='sticky'>
-	 <option value='yes'".($sticky ? " selected='selected'" : '').">{$lang['mod_mp_yes']}</option>
-	 <option value='no' ".($sticky ? '' : " selected='selected'").">{$lang['mod_mp_no']}</option>
-	 </select>
-	 </td>
-	 </tr>
-	 <tr>
-	 <td class='rowhead'>{$lang['mod_mp_lck']}</td>
-	 <td>
-	 <select name='locked'>
-	 <option value='yes'".($locked ? " selected='selected'" : '').">{$lang['mod_mp_yes']}</option>
-	 <option value='no'".($locked ? '' : " selected='selected'").">{$lang['mod_mp_no']}</option>
-	 </select>
-	 </td>
-	 </tr>
-	 <tr>
-	 <td class='rowhead'>{$lang['mod_mp_name']}</td>
-	 <td>
-	 <input type='text' name='topic_name' size='60' maxlength='{$Multi_forum['configs']['maxsubjectlength']}' value='".htmlsafechars($subject)."'>
-	 </td>
-	 </tr>
-	 <tr>
-	 <td class='rowhead'>{$lang['mod_mp_move']}</td>
-	 <td>
-	 <select name='new_forumid'>";
-    ($res = sql_query("SELECT id, name, min_class_write FROM forums ORDER BY name")) || sqlerr(__FILE__, __LINE__);
-    while ($arr = $res->fetch_assoc()) {
-        if ($CURUSER['class'] >= $arr["min_class_write"]) {
-            $HTMLOUT .= '<option value="'.(int)$arr["id"].'"'.($arr["id"] == $forumid ? ' selected="selected"' : '').'>'.htmlsafechars($arr["name"]).'</option>';
-        }
-    }
-    $HTMLOUT .= "</select>
-	 </td></tr>
-	 <tr>
-	 <td class='rowhead' style='white-space:nowrap;'>{$lang['mod_mp_del']}</td>
-	 <td>
-    <select name='delete'>
-	 <option value='no' selected='selected'>{$lang['mod_mp_no']}</option>
-	 <option value='yes'>{$lang['mod_mp_yes']}</option>
-	 </select>
-	 <br>
-	 {$lang['mod_mp_note']}
-	 </td>
-	 </tr>
-	 <tr>
-	 <td colspan='2' align='center'>
-	 <input type='submit' class='btn btn-primary' value='{$lang['mod_mp_uptop']}'>
-	 </td>
-	 </tr>";
-    $HTMLOUT .= "</table>";
-    /*$HTMLOUT .= end_table();*/
-    $HTMLOUT .= "</form>";
+	$HTMLOUT .= '<form method="post" action="forums.php">
+		<input type="hidden" name="action" value="updatetopic">
+		<input type="hidden" name="topicid" value="'.$topicid.'">
+		<div class="card">
+			<div class="card-divider">'.$lang['mod_mp_opt'].'</div>
+			<div class="card-section"> 
+				<div class="input-group">
+					<span class="input-group-label">'.$lang['mod_mp_stk'].'</span>
+					<select class="input-group-field" name="sticky">
+						<option value="yes" '.($sticky ? ' selected="selected"' : '').'>'.$lang['mod_mp_yes'].'</option>
+						<option value="no" '.($sticky ? '' : ' selected="selected"').'>'.$lang['mod_mp_no'].'</option>
+					</select>
+				</div>
+				<div class="input-group">
+					<span class="input-group-label">'.$lang['mod_mp_lck'].'</span>
+					<select class="input-group-field" name="locked">
+						<option value="yes" '.($locked ? ' selected="selected"' : '').'>'.$lang['mod_mp_yes'].'</option>
+						<option value="no" '.($locked ? '' : ' selected="selected"').'>'.$lang['mod_mp_no'].'</option>
+					</select>
+				</div>
+				<div class="input-group">
+					<span class="input-group-label">'.$lang['mod_mp_name'].'</span>
+					<input class="input-group-field" type="text" name="topic_name" size="60" maxlength="'.$Multi_forum['configs']['maxsubjectlength'].' value="'.htmlsafechars($subject).'">
+				</div>
+				<div class="input-group">
+					<span class="input-group-label">'.$lang['mod_mp_move'].'</span>
+					<select class="input-group-field" name="new_forumid">';
+					($res = sql_query("SELECT id, name, min_class_write FROM forums ORDER BY name")) || sqlerr(__FILE__, __LINE__);
+					while ($arr = $res->fetch_assoc()) {
+						if ($CURUSER['class'] >= $arr["min_class_write"]) {
+							$HTMLOUT .= '<option value="'.(int)$arr["id"].'"'.($arr["id"] == $forumid ? ' selected="selected"' : '').'>'.htmlsafechars($arr["name"]).'</option>';
+						}
+					}
+					$HTMLOUT .= '</select>
+				</div>
+				<div class="input-group">
+					<span class="input-group-label">'.$lang['mod_mp_del'].'</span>
+					<select class="input-group-field" name="delete">
+						<option value="no" selected="selected">'.$lang['mod_mp_no'].'</option>
+						<option value="yes">'.$lang['mod_mp_yes'].'</option>
+					</select>
+				</div>
+				<br>
+				<div class="callout alert">
+	 				<p>'.$lang['mod_mp_note'].'</p>
+				</div>
+	 			<input type="submit" class="button float-center" value="'.$lang['mod_mp_uptop'].'">
+			</div>
+		</div>
+	</form>';
 }
