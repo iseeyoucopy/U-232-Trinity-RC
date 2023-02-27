879f2d07aba73a054a9f93828a2eba27e38cdc98
rootKIDSMainOne <daniel.u.pedersen@hotmail.com>
2023-02-27 11:23:54 +0100

    Lang Updates / Danish

---
+++ lang/3/lang_achievementbonus.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * -------   U-232 Codename Trinity   ----------*
+ * ---------------------------------------------*
+ * --------  @authors U-232 Team  --------------*
+ * ---------------------------------------------*
+ * -----  @site https://u-232.duckdns.org/  ----*
+ * ---------------------------------------------*
+ * -----  @copyright 2020 U-232 Team  ----------*
+ * ---------------------------------------------*
+ * ------------  @version V6  ------------------*
+ */
+$lang = [
+
+    'achbon_err' => 'Fejl',
+    // 'achbon_err' => 'Fejl',
+    // 'achbon_err' => 'Error',
+
+    'achbon_off' => 'Det ser ud til, at præstationssystemet er offline.',
+    // 'achbon_off' => 'Det ser ud til, at præstationssystemet er offline.',
+    // 'achbon_off' => 'It appears that the achievement system is offline.',
+
+    'achbon_std_head' => "Tilfældig præstationsbonus",
+    // 'achbon_std_head' => "Tilfældig præstationsbonus",
+    // 'achbon_std_head' => "Achievement Random Bonus",
+
+    'achbon_no_ach_bon_pnts' => "Ingen præstationsbonuspoint",
+    // 'achbon_no_ach_bon_pnts' => "Ingen præstationsbonuspoint",
+    // 'achbon_no_ach_bon_pnts' => "No Achievement Bonus Points",
+
+    'achbon_no_ach_bon_pnts_msg' => "Det ser ud til, at du i øjeblikket ikke har nogen præstationsbonuspoint at bruge.",
+    // 'achbon_no_ach_bon_pnts_msg' => "Det ser ud til, at du i øjeblikket ikke har nogen præstationsbonuspoint at bruge.",
+    // 'achbon_no_ach_bon_pnts_msg' => "It appears that you currently have no Achievement Bonus Points available to spend.",
+
+    'achbon_congratulations' => "Tillykke",
+    // 'achbon_congratulations' => "Tillykke",
+    // 'achbon_congratulations' => "Congratulations",
+
+    'achbon_you_hv_just_won' => "du har lige vundet",
+    // 'achbon_you_hv_just_won' => "du har lige vundet",
+    // 'achbon_you_hv_just_won' => "you have just won",
+
+    'achbon_your_dl_been_reset_0' => "din downloadede total er blevet nulstillet fra en negativ værdi tilbage til 0",
+    // 'achbon_your_dl_been_reset_0' => "din downloadede total er blevet nulstillet fra en negativ værdi tilbage til 0",
+    // 'achbon_your_dl_been_reset_0' => "your downloaded total has been reset from a negative value back to 0",
+
+    'achbon_failed_msg1' => "Dunk64 er lige kørt over dig med sin ultradrevne kørestol. Bedre held næste gang.",
+    // 'achbon_failed_msg1' => "Dunk64 er lige kørt over dig med sin ultradrevne kørestol. Bedre held næste gang.",
+    // 'achbon_failed_msg1' => "Dunk64 has just run over you with his ultra-powered wheelchair. Better luck next time.",
+
+    'achbon_failed_msg2' => "Vi lægger dit præstationsbonuspoint på indsamlingspladen i et forsøg på at få Ducktape til en prostitueret.",
+    // 'achbon_failed_msg2' => "Vi lægger dit præstationsbonuspoint på indsamlingspladen i et forsøg på at få Ducktape til en prostitueret.",
+    // 'achbon_failed_msg2' => "We put your achievement bonus point into the collection plate in an attempt to get Ducktape a prostitute.",
+
+    'achbon_failed_msg3' => "Den onde skurk Adam har stjålet dit bonuspoint.",
+    // 'achbon_failed_msg3' => "Den onde skurk Adam har stjålet dit bonuspoint.",
+    // 'achbon_failed_msg3' => "The evil villian Adam has stolen your bonus point.",
+
+    'achbon_failed_msg4' => "Somehelp har brugt dit præstationsbonuspoint i forsøget på at købe hvalpemad til at lokke hunde til sin middagstallerken.",
+    // 'achbon_failed_msg4' => "Somehelp har brugt dit præstationsbonuspoint i forsøget på at købe hvalpemad til at lokke hunde til sin middagstallerken.",
+    // 'achbon_failed_msg4' => "Somehelp has used your achievement bonus point in attempt to buy puppy chow to lure doggies into his dinner plate.",
+
+    'achbon_failed_msg5' => "Hoodini har på magisk vis fået dit præstationsbonuspoint til at forsvinde, bedre held næste gang.",
+    // 'achbon_failed_msg5' => "Hoodini har på magisk vis fået dit præstationsbonuspoint til at forsvinde, bedre held næste gang.",
+    // 'achbon_failed_msg5' => "Hoodini has magically made your achievement bonus point dissapear, better luck next time.",
+
+    'achbon_random_achievement_bonus' => "Tilfældig præstationsbonus",
+    // 'achbon_random_achievement_bonus' => "Tilfældig præstationsbonus",
+    // 'achbon_random_achievement_bonus' => "Random Achievement Bonus",
+
+];
+?>
